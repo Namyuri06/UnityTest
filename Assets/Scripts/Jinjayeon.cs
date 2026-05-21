e1d3f1a4549c698c4198a71f5e8ddb2aa01b47aa
jjayn_o <uuha00@naver.com>
2026-05-21 11:06:16 +0900

    진자연 8주차 과제 제출

---
+++ Assets/Scripts/Jinjayeon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// 파일 이름 = 클래스 이름 반드시 동일해야 함
+public class Jinjayeon : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("GitHub 테스트 성공: Start()");
+    }
+
+    void Update()
+    {
+
+    }
+}
